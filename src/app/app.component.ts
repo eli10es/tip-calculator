986c83b47f843a36819894cb703f4eda54ebfbd1
Elisei <eli10es@yahoo.es>
2024-09-13 15:30:16 +0300

    added the suggestion table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CalculatorComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CalculatorComponent,
+    SuggestionsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
